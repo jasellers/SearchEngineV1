4dfc69a4d014cf1a44ee8db966349988a4c31e0f
Nick Evans <nicholas-evans@uiowa.edu>
2014-11-06 11:16:14 -0600

    Allocate methods

--- org/uiowa/cs2820/engine/Allocate.java
+++ org/uiowa/cs2820/engine/Allocate.java
@@ -6,27 +6,36 @@ public class Allocate {
 
 	private BitSet allocationSet;
 	
-	
 	public Allocate() {
-		
+		allocationSet = new BitSet();
 		//Checkpoint.restore(file)
 		
 	}
 	
 	// Changed method name to be different from class/constructor name
 	public int allocateArea() {
+		int areaNumber;
+		if (allocationSet.isEmpty()) {
+			areaNumber = 0;
+		} else { // Initially using a crude method to get the next available area...
+			areaNumber = allocationSet.nextClearBit(0);
+		}
+		// and mark it as now in use
+		allocationSet.set(areaNumber);
 		
-		//Checkpoint.save(file)
-		int next = 0;
-		//next = allocationSet.nextClearBit(fromIndex);
-		return next;
+		//Checkpoint.save(file)		
+		return areaNumber;
 	}
 	
 	// Changed to match previous method
 	public void freeArea(int areaNumber) {
-
+		//To throw an exception if no areas are allocated or no?
 			allocationSet.clear(areaNumber);
 		
 		//Checkpoint.save(file)
+	}
+	
+	public int size() {
+		return allocationSet.cardinality();
 	}
 }
